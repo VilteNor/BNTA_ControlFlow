d8ec93fd53c38a2c201b78fc7e71ed0cdf5e0988
Vilte N <viltenor@gmail.com>
2022-08-12 09:03:35 +0100

    MVP completed, extension started

--- src/main/java/SecretNum.java
+++ src/main/java/SecretNum.java
@@ -1,24 +1,29 @@
+import java.util.Scanner;
+
 public class SecretNum {
     public static void main(String[] args) {
-
-
 
 //        Create a simple number guessing game, that takes in some user input matches it to a 'secret' number.
 //        -- it returns a string, so find out how to convert.
 //                The program should indicate if an incorrect guess is too high or too low. It should, of course,
 //                let the user know if they have guessed the number correctly.
 
+        int secretNum = 6;
+        System.out.println("guess the secret number!");
+        Scanner reader = new Scanner(System.in);
+        int guessNum = reader.nextInt();
+//        alternatively:
+//        String guess = reader.nextLine();
+//        int guessNum = Integer.parseInt(guess);
 
-
-        int secretNum = 10;
-
-
-
-
-
-
-
-
-
+        if (guessNum==secretNum){
+            System.out.println("correct!");
+        }
+        else if (guessNum<secretNum) {
+            System.out.println("your guess is too low");
+        }
+        else {
+            System.out.println("your guess is too high");
+        }
     }
 }
--- src/main/java/WhatToWear.java
+++ src/main/java/WhatToWear.java
@@ -1,13 +1,41 @@
+import java.util.Scanner;
+
 public class WhatToWear {
     public static void main(String[] args) {
 
-//        extension task
+//        Allow your program to collect some descriptive user input about the weather ("rainy", "sunny", "cloudy", "snow", etc)
+//        and the average temperature for the day (8, 18, 28, etc).
+//        Let your program output clothing suggestions depending on a combination of weather conditions and
+//        temperature.
+//         e.g. if it is both "rainy" and less than 12 degrees, output "wear a waterproof winter coat"
+
+        System.out.println("how does the weather seem? (e.g. rainy, sunny, cloudy, snowy)");
+        Scanner reader = new Scanner(System.in);
+        String weatherInfo = reader.nextLine();
+        System.out.println("what is the average temperature today?");
+        int temperature = reader.nextInt();
+
+        if (weatherInfo.equals("rainy") && temperature<=12){
+            System.out.println("wear a waterproof winter coat.");
+        }
+        else if (weatherInfo.equals("rainy") && temperature>13){
+            System.out.println("wear a light waterproof coat.");
+        }
+        else if (weatherInfo.equals("sunny") && temperature<=12){
+            System.out.println("wear a winter coat.");
+        }
+        else if (weatherInfo.equals("sunny") && temperature<=20){
+            System.out.println("wear a light jacket.");
+        }
+        else if (weatherInfo.equals("sunny") && temperature>21){
+            System.out.println("wear summer clothes, it's hot outside.");
+        }
 
 
 
-
-
-
+        else {
+            System.out.println("your guess is too high");
+        }
+    }
 
     }
-}
